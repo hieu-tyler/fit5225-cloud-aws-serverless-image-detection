4428c3b1c578a9ec059770f69674132803c27826
Kim Berhanu <141489735+kberhanu@users.noreply.github.com>
2024-05-30 09:53:39 +1000

    delete api

--- api_2_muskaan.js
+++ api_2_muskaan.js
@@ -1,5 +1,7 @@
 const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
+
 exports.handler = async (event, context, callback) => {
   console.log("remaining time =", context.getRemainingTimeInMillis());
   console.log("functionName =", context.functionName);
@@ -8,6 +10,9 @@ exports.handler = async (event, context, callback) => {
   let statusCode = "200";
   const headers = { "Content-Type": "application/json" };
   const tableName = "image_info";
+  const s3ImageBucket = "ass3imageupload";
+  const s3ThumbnailBucket = "ass3thumbnailbucketpixtag";
+
   try {
     switch (event.httpMethod) {
       case "GET":
@@ -23,13 +28,65 @@ exports.handler = async (event, context, callback) => {
               ":thumbnail_url": event.queryStringParameters.thumbnail_url,
             },
           };
-          //body = await dynamo.scan(params).promise();
           const response = await dynamo.scan(params).promise();
           body = response.Items.map((item) => item.s3_url);
+        } else {
+          body = { worked: "no" };
         }
-        //body = await dynamo.scan({ TableName: tableName }).promise();
-        else body = { worked: "no" };
         break;
+
+      case "DELETE":
+        if (event.body) {
+          const requestBody = JSON.parse(event.body);
+          const thumbnailUrls = requestBody.thumbnail_urls;
+
+          for (let thumbnailUrl of thumbnailUrls) {
+            // Query DynamoDB to get the S3 URL for each thumbnail URL
+            let params = {
+              TableName: tableName,
+              ProjectionExpression: "s3_url",
+              FilterExpression: "thumbnail_url = :thumbnail_url",
+              ExpressionAttributeValues: {
+                ":thumbnail_url": thumbnailUrl,
+              },
+            };
+            const response = await dynamo.scan(params).promise();
+            const items = response.Items;
+
+            for (let item of items) {
+              const s3Url = item.s3_url;
+
+              // Delete the image from the S3 image bucket
+              const s3ImageParams = {
+                Bucket: s3ImageBucket,
+                Key: s3Url.split('/').pop() // Extracting the file name from the URL
+              };
+              await s3.deleteObject(s3ImageParams).promise();
+
+              // Delete the thumbnail from the S3 thumbnail bucket
+              const s3ThumbnailParams = {
+                Bucket: s3ThumbnailBucket,
+                Key: thumbnailUrl.split('/').pop() // Extracting the file name from the URL
+              };
+              await s3.deleteObject(s3ThumbnailParams).promise();
+
+              // Delete the entry from DynamoDB
+              const deleteParams = {
+                TableName: tableName,
+                Key: {
+                  "thumbnail_url": thumbnailUrl
+                }
+              };
+              await dynamo.delete(deleteParams).promise();
+            }
+          }
+          body = { message: "Images and thumbnails deleted successfully" };
+        } else {
+          statusCode = "400";
+          body = { error: "No body provided" };
+        }
+        break;
+
       default:
         throw new Error(`Unsupported method "${event.httpMethod}"`);
     }
